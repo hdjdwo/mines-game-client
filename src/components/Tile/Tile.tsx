5b1315295f616614fcdeee9fc4cc0926f5b7c3c7
hdjdwo <vlad.1488skaskevich@gmail.com>
2026-01-10 00:06:33 +0300

    feature: epic glow around tile

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -4,6 +4,7 @@ import mineTile from '../../assets/image/mine.png'
 import noMineTile from '../../assets/image/noMine.png'
 import type { FC} from 'react'
 import type { TileState } from '../../Context/GameActions'
+import { useGame } from '../../Context/GameHoks'
 
 interface TileProps extends TileState {
   onTileClick: () => void;
@@ -12,17 +13,33 @@ interface TileProps extends TileState {
 
 
 export const Tile: FC<TileProps> = ({ isOpen, tileStatus, onTileClick, isExploded}) => {
- 
-  return (
+ const { payoutTable, field, gameStatus } = useGame();
+
+  const currentStep = field.filter(tile => tile.isOpen && tile.tileStatus === 'EMPTY').length;
+  const currentMultiplier = payoutTable.find(i => i.step === currentStep)?.multiplier || 0;
+
+  const isGameActive = gameStatus === 'STARTED' || gameStatus === 'CONTINUES';
+  const shouldGlow = isGameActive && !isOpen;
+  
+  const isFire = shouldGlow && currentMultiplier >= 10 && currentMultiplier < 50;
+  const isEpic = shouldGlow && currentMultiplier >= 50;
+
+return (
     <div
-     className={isExploded ? classes.tile_exploded : classes.tile }
-     onClick={onTileClick}
-     >
-      
+      className={`
+        ${classes.tile}
+        ${isExploded ? classes.tile_exploded : ''}
+        ${isFire ? classes.tile_fire : ''}
+        ${isEpic ? classes.tile_epic : ''}
+ 
+      `}
+      onClick={onTileClick}
+    >
       <img
-      className={classes.tileBackStage}
-      src={isOpen ? (tileStatus === 'MINE' ? mineTile : noMineTile) : backStageTile }
-      alt="" />
+        className={classes.tileBackStage}
+        src={isOpen ? (tileStatus === 'MINE' ? mineTile : noMineTile) : backStageTile}
+        alt=""
+      />
     </div>
-  )
-}
+  );
+};
